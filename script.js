5a8f93a04df67a72163a9ec9a18234d4996584fd
Yura-Yaremko <yurezyaremko@gmail.com>
2021-03-23 19:11:13 +0200

    Revealing elements

--- script.js
+++ script.js
@@ -177,6 +177,27 @@ headerObserver.observe(header);
 
 ////////////////////////////
 
+//Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 // const btnScrollTo = document.querySelector('.btn--scroll-to');
 // const section1 = document.querySelector('#section--1');
 
